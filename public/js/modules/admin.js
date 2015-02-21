5e6e53815344a20835d06021ad060a7d25689ca9
Doron Brikman <brikmand@gmail.com>
2015-02-21 17:01:05 +0200

    add the admin graph there is still a problem with the select element, that it is not display the year after selection

--- public/js/modules/admin.js
+++ public/js/modules/admin.js
@@ -1,4 +1,4 @@
-angular.module('TicTacToe.statistics', ['uiGmapgoogle-maps'])
+angular.module('TicTacToe.statistics', ['uiGmapgoogle-maps', 'nvd3ChartDirectives'])
 
     .factory('dataService', ['$http', 'serverData', function ($http,serverData) {
 
@@ -21,115 +21,51 @@ angular.module('TicTacToe.statistics', ['uiGmapgoogle-maps'])
     }])
 
     .controller('GraphsCtrl', ['$scope','dataService', function ($scope, dataService) {
-
-        this.usersPerMonth = [];
-
-        $scope.$on('$destroy', function() {
-            d3.select("svg").remove();
-        });
-
-        var that = this;
-
-        this.setGraph = function(){
-            d3.select("svg").remove();
-            drawGraph(this.selectedYear.monthly)
-        }
+        var vm = this;
 
         dataService.getUsersPerMonth(function(result){
-            that.usersPerMonth = result;
+            vm.usersPerMonth = [{
+                key: result[result.length - 1],
+                values: dataForGraph(result[result.length - 1].monthly)
+            }];
 
-            that.selectedYear = result[0];
-            that.setGraph();
+            vm.allData = result;
+
+            vm.selectedYear = result[result.length - 1];
         });
+        
+        vm.setGraph = function () {
+            dataService.getUsersPerMonth(function(result){
+                vm.usersPerMonth = [{
+                    key: vm.selectedYear._id,
+                    values: dataForGraph(vm.selectedYear.monthly)
+                }];
 
-        function drawGraph(monthly){
+                vm.allData = result;
 
-            var data = [
-                {month: "January", UserRegistered: 0},
-                {month: "February", UserRegistered:0},
-                {month: "March", UserRegistered: 0},
-                {month: "April", UserRegistered: 0},
-                {month: "May", UserRegistered: 0},
-                {month: "June", UserRegistered: 0},
-                {month: "July", UserRegistered: 0},
-                {month: "August", UserRegistered: 0},
-                {month: "September", UserRegistered: 0},
-                {month: "October", UserRegistered: 0},
-                {month: "November", UserRegistered: 0},
-                {month: "December", UserRegistered: 0}
-            ];
-
-            for (i = 0; i <= monthly.length - 1; i++) {
-                data[monthly[i].month - 1].UserRegistered += monthly[i].UserRegistered;
-            }
-
-            var margin = {top: 40, right: 20, bottom: 30, left: 40},
-                width = 960 - margin.left - margin.right,
-                height = 500 - margin.top - margin.bottom;
-
-            var formatPercent = d3.format("d");
-
-            var x = d3.scale.ordinal()
-                .rangeRoundBands([0, width], .1);
-
-            var y = d3.scale.linear()
-                .range([height, 0]);
-
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("bottom");
-
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                .orient("left")
-                .tickFormat(formatPercent);
-
-            var tip = d3.tip()
-                .attr('class', 'd3-tip')
-                .offset([-10, 0])
-                .html(function(d) {
-                    return "<strong>User Registered:</strong> <span style='color:red'>" + d.UserRegistered + "</span>";
-                })
-
-            var svg = d3.select("body").append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            svg.call(tip);
-
-            // The following code was contained in the callback function.
-            x.domain(data.map(function(d) { return d.month; }));
-            y.domain([0, d3.max(data, function(d) { return d.UserRegistered; })]);
-
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("User Registered");
-
-            svg.selectAll(".bar")
-                .data(data)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) { return x(d.month); })
-                .attr("width", x.rangeBand())
-                .attr("y", function(d) { return y(d.UserRegistered); })
-                .attr("height", function(d) { return height - y(d.UserRegistered); })
-                .on('mouseover', tip.show)
-                .on('mouseout', tip.hide)
+                $scope.$apply();
+            });
         };
 
+        function dataForGraph (monthly) {
+            var values = [];
+            var index = 1;
+            monthly.forEach(function(mon) {
+                for (var i = index; i < mon.month; i++) {
+                    values.push([i, 0]);
+                }
+                index = mon.month;
+                values.push([index, mon.UserRegistered]);
+            });
+
+            if (index < 12) {
+                for (var i = index + 1; i < 13; i++) {
+                    values.push([i, 0]);
+                }
+            }
+
+            return values;
+        }
     }])
 
     .controller('MapCtrl', ['dataService', function (dataService ) {
